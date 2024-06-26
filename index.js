5005f8fa4e2c91730a7169d8ada668c8b6a46e4f
Nahid Hasan <nahidjc80@gmail.com>
2024-06-26 20:38:40 +0600

    Initial commit with response handler, tracing middleware, and error response handler

---
+++ index.js
@@ -0,0 +1,17 @@
+const { responseHandler } = require("./lib/responseHandler");
+const { errorResponseHandler } = require("./lib/errorResponseHandler");
+const { statusCodes } = require("./lib/statusCodes");
+const { errorCodes } = require("./lib/errorCodes");
+const initJaegerTracer = require("./lib/tracer");
+const tracingMiddleware = require("./lib/tracingMiddleware");
+const dateTimeHelpers = require("./lib/dateTimeHelpers");
+
+module.exports = {
+  responseHandler,
+  errorResponseHandler,
+  statusCodes,
+  errorCodes,
+  initJaegerTracer,
+  tracingMiddleware,
+  dateTimeHelpers,
+};
